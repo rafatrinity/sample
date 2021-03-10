340abfe103e1edb39893c9cbd8663b0d0ed39033
rafael trindade <rafaeltrindade649@gmail.com>
2021-03-10 13:32:45 -0300

    auth e url ok

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+import { Md5 } from 'ts-md5/dist/md5';
+import { PRIVATE_KEY, PUBLIC_KEY } from './secrets.key';
+var timestamp = Number(new Date());
+var hash = Md5.hashStr(`${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`);
 export const environment = {
-  production: true
+  production: true,
+  baseUrl: `https://gateway.marvel.com:443/v1/public/`,
+  auth: `?ts=${timestamp}&apikey=${PUBLIC_KEY}&hash=${hash}`,
+  settings: {
+    appErrorPrefix: '[Sample Error] ',
+    appTitle: 'Sample',
+    version: '1.0.0',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
+import { Md5 } from 'ts-md5/dist/md5';
+import { PRIVATE_KEY, PUBLIC_KEY } from './secrets.key';
+var timestamp = Number(new Date());
+var hash = Md5.hashStr(`${timestamp}${PRIVATE_KEY}${PUBLIC_KEY}`);
 export const environment = {
-  production: false
+  production: false,
+  baseUrl: `https://gateway.marvel.com:443/v1/public/`,
+  auth: `?ts=${timestamp}&apikey=${PUBLIC_KEY}&hash=${hash}`,
+  settings: {
+    appErrorPrefix: '[Sample Error] ',
+    appTitle: 'Sample',
+    version: '1.0.0',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
