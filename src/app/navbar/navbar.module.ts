fbf22e79166a1a81b636ed66c110c512a110e430
rafael trindade <rafaeltrindade649@gmail.com>
2021-03-14 23:57:14 -0300

    add icones

--- src/app/navbar/navbar.module.ts
+++ src/app/navbar/navbar.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
-
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @NgModule({
   declarations: [NavbarComponent],
-  imports: [CommonModule, MatToolbarModule],
+  imports: [CommonModule, MatToolbarModule, FontAwesomeModule],
   exports: [NavbarComponent],
 })
 export class NavbarModule {}
--- src/app/navbar/navbar/navbar.component.ts
+++ src/app/navbar/navbar/navbar.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from 'src/environments/environment';
+import { faGamepad, faChartPie } from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  faGamepad = faGamepad;
+  faChartPie = faChartPie;
+  env = environment.assets;
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
