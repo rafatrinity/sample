c2423000eead11d688a78e0137d3d548ec878e78
rafael trindade <rafaeltrindade649@gmail.com>
2021-03-17 16:43:04 -0300

    criando estrutura

---
+++ src/app/puzzle/puzzle.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PuzzleComponent } from './puzzle/puzzle.component';
+
+
+
+@NgModule({
+  declarations: [PuzzleComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class PuzzleModule { }
---
+++ src/app/puzzle/puzzle/puzzle.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-puzzle',
+  templateUrl: './puzzle.component.html',
+  styleUrls: ['./puzzle.component.css']
+})
+export class PuzzleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  puzzleAsk(resp: string){
+    
+  }
+
+}
