23b074393641d51f08ba1bdf4922ed73f08ad13e
rafael trindade <rafaeltrindade649@gmail.com>
2021-03-18 02:04:42 -0300

    design quase ok

--- src/app/puzzle/puzzle.module.ts
+++ src/app/puzzle/puzzle.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PuzzleComponent } from './puzzle/puzzle.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [PuzzleComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, MatButtonModule],
   exports: [PuzzleComponent],
 })
 export class PuzzleModule {}
