6c7c389049e2cf50bad304db7830994f0ef470e7
rafael trindade <rafaeltrindade649@gmail.com>
2021-03-18 02:20:51 -0300

    deploy

--- src/app/puzzle/puzzle/puzzle.component.ts
+++ src/app/puzzle/puzzle/puzzle.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-puzzle',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./puzzle.component.scss'],
 })
 export class PuzzleComponent implements OnInit {
+  env = environment.assets;
   ask: string = 'Is that number even?';
   even: number = 0;
   odd: number = 0;
