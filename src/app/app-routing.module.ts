84afe48162950a1b02f71c0dc8ffb7cf4bf67d1a
rafael trindade <rafaeltrindade649@gmail.com>
2021-03-21 21:05:40 -0300

    arrumando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CatalogComponent } from './characters/catalog/catalog.component';
-import { PuzzleComponent } from './puzzle/puzzle/puzzle.component'
+import { PuzzleComponent } from './puzzle/puzzle/puzzle.component';
 const routes: Routes = [
-  {path:'catalog', component: CatalogComponent},
-  {path:'puzzle', component: PuzzleComponent},
+  { path: 'catalog', component: CatalogComponent },
+  { path: 'puzzle', component: PuzzleComponent },
   { path: '', redirectTo: '/puzzle', pathMatch: 'full' },
+  // {path:'404', component: 404Component},
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
